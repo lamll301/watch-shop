74f649ef53c027c5dce04604258c1d3b8defa30a
Le Ly Lam <108975594+LeLyLam@users.noreply.github.com>
2023-10-26 19:22:19 +0700

    mongodb

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1/watch_shop_dev');
+        await mongoose.connect('mongodb+srv://root:1234@cluster0.pnngwnu.mongodb.net/');
+        // await mongoose.connect('mongodb://127.0.0.1/watch_shop_dev');
         console.log('Connect successfully!!!');
     }
     catch (error) {
